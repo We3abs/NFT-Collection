3bd3aa10752bbc2e8d25ceedb689b9b9ed705146
Unreal <t.babinos@gmail.com>
2023-02-01 14:23:01 +0200

    Initial Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x681f781f05c8ea5dfc87395e5d4356ab981dff81";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
